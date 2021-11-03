9244ffb29b1edc7fe13fd831d01bc13326d7659b
Gerald de Jong <geralddejong@gmail.com>
2021-11-03 15:52:41 +0100

    Full height

--- davinci-dome/src/App.js
+++ davinci-dome/src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
-import {useEffect, useRef} from "react";
-
 
 function Box(props) {
     return (
         <mesh {...props}>
-
             <boxGeometry args={[1, 1, 1]}/>
             <meshStandardMaterial color="red"/>
         </mesh>
-
     )
 }
 
-
 function App() {
-
     return (
         <div className="App">
-            <Canvas>
+            <Canvas className="Canvas">
                 <ambientLight/>
                 <pointLight position={[10, 10, 10]}/>
                 <Box position={[-1.2, 0, 1]}/>
