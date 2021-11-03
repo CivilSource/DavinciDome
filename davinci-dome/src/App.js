1a93033cbb16b9b5aa4728c618fa3fa5d8989581
BuroCiviel <75376761+BuroCiviel@users.noreply.github.com>
2021-11-03 15:44:29 +0100

    react-three/drei added and orbit controls
    react-three/drei added and orbit controls

--- davinci-dome/src/App.js
+++ davinci-dome/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
+import {useEffect, useRef} from "react";
+
+
+function Box(props) {
+    return (
+        <mesh {...props}>
+
+            <boxGeometry args={[1, 1, 1]}/>
+            <meshStandardMaterial color="red"/>
+        </mesh>
+
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Canvas>
+                <ambientLight/>
+                <pointLight position={[10, 10, 10]}/>
+                <Box position={[-1.2, 0, 1]}/>
+                <Box position={[1.2, 0, 0]}/>
+                <PerspectiveCamera makeDefault={true} position={[1.2, 1, 2]}/>
+                <OrbitControls/>
+            </Canvas>
+        </div>
+    )
 }
 
 export default App;
