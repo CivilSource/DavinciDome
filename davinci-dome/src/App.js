9e576e81fdf76ccafac6df832f28770f034a4c99
BuroCiviel <75376761+BuroCiviel@users.noreply.github.com>
2021-11-03 16:34:50 +0100

    Loop of Balls

--- davinci-dome/src/App.js
+++ davinci-dome/src/App.js
@@ -11,14 +11,34 @@ function Box(props) {
     )
 }
 
+function Ball(props) {
+    return (
+        <mesh {...props}>
+            <sphereGeometry args={[0.25, 32, 16]}/>
+            <meshPhongMaterial color="blue"/>
+        </mesh>
+    )
+}
+
 function App() {
+    const balls = []
+    const radius = 10
+    for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 15) {
+        const x = Math.sin(angle) * radius
+        const z = Math.cos(angle) * radius
+        balls.push([x, z])
+    }
     return (
         <div className="App">
             <Canvas className="Canvas">
                 <ambientLight/>
                 <pointLight position={[10, 10, 10]}/>
-                <Box position={[-1.2, 0, 1]}/>
-                <Box position={[1.2, 0, 0]}/>
+                <Box position={[0, 0, 0]}/>
+                {
+                    balls.map(xz => {
+                        return <Ball position={[xz[0], 0, xz[1]]}/>
+                    })
+                }
                 <PerspectiveCamera makeDefault={true} position={[1.2, 1, 2]}/>
                 <OrbitControls/>
             </Canvas>
